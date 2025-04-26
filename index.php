27937c0202ce369b3ee1d3f9ef45183952423c88
Jerdon07 <jdlitaoen@gmail.com>
2025-04-26 20:03:06 +0800

    Masive Changes in the Body Element

--- index.php
+++ index.php
@@ -1,43 +1,19 @@
-<?php include "database/config.php"; ?>
+<?php include "./database/config.php"; ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="./src/output.css">
     <title>Document</title>
 </head>
-<body>
-    <h2>USERS</h2>
 
-    <a href="src/create.php">Add New User</a>
+<body class="text-center bg-blue-100 flex justify-center items-center h-screen">
 
-    <table border="1" cellpadding"10"></table>
+    <div class="h-100 w-100 bg-white rounded-md">
+        <h3>TEST</h3>
+    </div>
 
-    <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Gmail</th>
-        <th>Password</th>
-    </tr>
-
-    <?php
-        $result = $conn->query("SELECT * FROM users");
-        while($row = $result->fetch_assoc()):
-    ?>
-
-    <tr>
-        <td><?= $row['id'] ?></td>
-        <td><?= $row['name'] ?></td>
-        <td><?= $row['gmail'] ?></td>
-        <td><?= $row['password'] ?></td>
-
-        <td>
-            <a href="src/update.php?id=<?= $row['id'] ?>">Edit</a>
-            <a href="src/delete.php?id=<?= $row['id'] ?>" onclick="return confirm('Delete this user?')">Delete</a>
-        </td>
-    </tr>
-    <?php endwhile; ?>
 </body>
 </html>
