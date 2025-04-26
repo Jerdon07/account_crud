0123057046b0486ae14ad3619cb0a1cb95f81745
Jerdon07 <jdlitaoen@gmail.com>
2025-04-26 21:00:34 +0800

    Added the POST Form

--- index.php
+++ index.php
@@ -11,8 +11,14 @@
 
 <body class="text-center bg-blue-100 flex justify-center items-center h-screen">
 
-    <div class="h-100 w-100 bg-white rounded-md">
-        <h3>TEST</h3>
+    <div class="h-100 w-100 bg-white rounded-xl p-5">
+        <h1 class="text-2xl font-semibold">LOGIN</h1>
+
+        <form action="" method="POST">
+            <input type="text" name="username" placeholder="Account" required class="h-20 w-20">
+            <input type="password" name="password" placeholder="Password" required>
+            <input type="submit" name="submit" value="Login" class="bg-blue-500 text-white rounded-lg p-2 mt-3">
+        </form>
     </div>
 
 </body>
