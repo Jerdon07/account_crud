7879f232fb7c635f7477d3a7ea9c0673c84d2900
Jerdon07 <jdlitaoen@gmail.com>
2025-05-04 20:19:57 +0800

    Updated the Style

--- index.php
+++ index.php
@@ -12,12 +12,12 @@
 <body class="text-center bg-blue-100 flex justify-center items-center h-screen">
 
     <div class="h-100 w-100 bg-white rounded-xl p-5">
-        <h1 class="text-2xl font-semibold">LOGIN</h1>
+        <h1 class="text-2xl font-semibold mb-5">LOGIN</h1>
 
-        <form action="" method="POST">
-            <input type="text" name="username" placeholder="Account" required class="h-20 w-20">
-            <input type="password" name="password" placeholder="Password" required>
-            <input type="submit" name="submit" value="Login" class="bg-blue-500 text-white rounded-lg p-2 mt-3">
+        <form action="" method="POST" class="bg-red-100">
+            <input type="text" name="username" placeholder="Account" required class="h-12 w-90 bg-gray-200 mb-5"><br>
+            <input type="password" name="password" placeholder="Password" required class="h-12 w-90 bg-gray-200 mb-5 pb-5"><br>
+            <input type="submit" name="submit" value="Login" class="bg-red-200 text-white rounded-sm p-2 mt-3">
         </form>
     </div>
 
