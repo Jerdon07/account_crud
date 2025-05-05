e676ff375f7c8004f43536a64b69c061159b15be
Jerdon07 <jdlitaoen@gmail.com>
2025-05-05 20:53:31 +0800

    Added the Users' Dashboard

---
+++ src/read.php
@@ -0,0 +1,18 @@
+<?php include "../database/config.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./output.css">
+    <title>Users</title>
+</head>
+<body class="bg-blue-100">
+    <div>
+        <h2>Users</h2>
+        <input type="button" value="Edit" class="h-12 w-90 bg-black text-white">
+        <input type="button" value="Delete" class="h-12 w-90 bg-black text-white">
+    </div>
+</body>
+</html>
